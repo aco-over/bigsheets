4c643bdc301ee9ed242235bd47d3a6d375638118
aco-over <aco.over.feed@gmail.com>
2016-09-21 18:25:47 -0700

    Init project

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+import {asdf} from '../index.js'
+
+describe('ViewHud', () => {
+  describe("Can mount", () => {
+    it('can render', () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
